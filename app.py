724d52c232e8bab3fe55817e5e03189442f3fb7b
yosiaazarya <yosia.official@gmail.com>
2022-12-01 14:04:41 +0700

    removing unnecessary things

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ def getMessage():
 @app.route("/")
 def webhook():
    bot.remove_webhook()
-   # TO DO: Edit the value of variable heroku_app_name according to your Heroku application name
+   # TO DO: Edit the value of variable railway_public_domain according to the railway public domain
    railway_public_domain = "https://web-production-c6f7.up.railway.app/"
 
    # NOTE: You need to use a publically available URL that the Telegram servers can reach.
-   bot.set_webhook(url=f'https://{railway_public_domain}/{TOKEN}')
+   bot.set_webhook(url=f'{railway_public_domain}/{TOKEN}')
    return "Bot is running", 200
 
 if __name__ == "__main__":
